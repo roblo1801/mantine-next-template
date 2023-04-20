d856a48ee96c277451aadc45a494670181fa82da
Robert Raikowski <roblo1801@gmail.com>
2023-04-20 07:16:42 -0400

    Update get-cart.js

--- pages/api/get-cart.js
+++ pages/api/get-cart.js
@@ -7,16 +7,12 @@ export default async function handler(req, res) {
 
     if (!userId) {
       const response = await createGuestCart();
-      console.log('Created guest cart with ID:', response);
-      const cartRef = db.collection('carts').doc(response);
+      const cartRef = db.collection('carts').doc(response.toString());
       const cart = await cartRef.get();
       const cartData = cart.data();
       const cartItems = cartData.items;
 
-      res
-        .status(200)
-        .setHeader('Set-Cookie', `userId=${response}; path=/; HttpOnly; Secure; SameSite=Strict`)
-        .json(cartItems);
+      res.status(200).setHeader('Set-Cookie', `userId=${response}; path=/`).json(cartItems);
     }
     const cartRef = db.collection('carts').doc(userId);
     const cart = await cartRef.get();
